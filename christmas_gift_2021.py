3233fd89367418604361845f4f519248fa1d497c
Timon Lorenz <TimmensOne@users.noreply.github.com>
2021-02-12 22:45:45 +0100

    rename

--- christmas_gift_2021.py
+++ christmas_gift_2021.py
@@ -1,28 +1,23 @@
 import random
 
-presents_list_example = [(1, 'Bernd'), (2, 'Timon'), (3, 'Bernd'), (4, 'Bernd'),
+gift_list_example = [(1, 'Bernd'), (2, 'Timon'), (3, 'Bernd'), (4, 'Bernd'),
                         (5, 'Lisa'), (6, 'Justin'), (7, 'Lisa'), (8, 'Lisa'), 
                         (9, 'Timon'), (10, 'Timon'), (11, 'Timon'), (12, 'Timon'), 
                         (13, 'Timon'), (14, 'Justin'), (15, 'Lisa'), (16, 'Timon'), 
                         (17, 'Justin'), (18, 'Justin'), (19, 'Justin'), (20, 'Justin'),
                         (21, 'Justin'), (22, 'Justin'), (23, 'Justin'), (24, 'Justin'),
-                        (25, 'Bernd
-                    '), (26, 'Timon'), (27, 'Justin'), (28, 'Justin'),
+                        (25, 'Bernd'), (26, 'Timon'), (27, 'Justin'), (28, 'Justin'),
                         (29, 'Justin')]
-presents_name_list = ['Bernd', 'Timon', 'Bernd', 'Bernd', 'Lisa', 'Justin', 'Lisa', 'Lisa', 'Timon', 
-                    'Timon', 'Timon', 'Timon', 'Timon', 'Justin', 'Lisa', 'Timon', 'Justin', 'Justin', 
-                    'Justin', 'Justin', 'Justin', 'Justin', 'Justin', 'Justin', 'Bernd
-                ', 'Timon', 'Justin',
-                    'Justin', 'Justin']
+gift_name_list = [e[1] for e in gift_list_example]
 name_list = ['Timon', 'Justin', 'Lisa', 'Bernd']
 count_list = []
 probability_list = []
 
 
-def count_presents_per_person(presents_list):
+def count_gift_per_person(gift_list):
     count = 0
     for i in range(len(name_list)):
-        for j in presents_list:
+        for j in gift_list:
             if(name_list[i] == j[1]):
                 count += 1
         single_tup = (count, name_list[i])
@@ -42,18 +37,18 @@ def get_interval():
     return intervals
 
 
-def set_present_sequence(interval_info):
+def set_gift_sequence(interval_info):
     dist_list = []
-    present_location = 0
-    present_name = ''
+    gift_location = 0
+    gift_name = ''
     for i in range(len(interval_info)):
-        present_name = interval_info[i][2]
+        gift_name = interval_info[i][2]
         count = int(interval_info[i][0])
         for j in range(count):
-            dist_tuple = (present_location, present_name)
+            dist_tuple = (gift_location, gift_name)
             dist_list.append(dist_tuple)
-            present_location += float(interval_info[i][1])
-        present_location = 0
+            gift_location += float(interval_info[i][1])
+        gift_location = 0
     dist_list.sort()
     # check double turn
     for i in range(len(dist_list)-1):
@@ -68,7 +63,7 @@ def create_id_list():
     id_list = []
     for i in name_list:
         ids = []
-        for j in presents_list_example:
+        for j in gift_list_example:
             if(i == j[1]):
                 ids.append(j[0])
         random.shuffle(ids)
@@ -78,8 +73,8 @@ def create_id_list():
 
 
 def create_distribution_list():
-    count_presents_per_person(presents_list_example)
-    distribution_order = set_present_sequence(get_interval())
+    count_gift_per_person(gift_list_example)
+    distribution_order = set_gift_sequence(get_interval())
     id_list = create_id_list()
     final_list = []
     for i in distribution_order:
